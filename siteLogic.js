89eec872fda497910f4464a1cdfc5178e2dde687
John Weber <johnhenryweber@gmail.com>
2019-10-03 07:49:53 -0700

    Almost Finished

--- siteLogic.js
+++ siteLogic.js
@@ -1,6 +1,5 @@
 var rawProjectArray = [];
 var completedProjects = [];
-var completedHTML;
 
 function createProject(title, description, github, website = null) {
   var project = {}
@@ -13,7 +12,7 @@ function createProject(title, description, github, website = null) {
 }
 
 function createProjectHtml(project) {
-  var $blankproject = $('<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title"></h3></div><div class="panel-body"><p id="introparagraph"></p><div class="gallery"><a href="" id="githubLink"><div class="desc"><b>Check Out the Github!</b></div><img src="./octocat.png" class="octocat"></a></div><div class="gallery"><a href="http://google.com" id="internetLink"><div class="desc"><b>Visit the Website!</b></div><img src="./internet.png" class="internet"></a></div></div></div>');
+  var $blankproject = $('<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title"></h3></div><div class="panel-body"><p id="introparagraph"></p><div class="gallery"><a href="" id="githubLink"><div class="desc"><b>Check Out the Github!</b></div><img src="./octocat.png" class="octocat"></a></div><div class="gallery internetGallery"><a href="http://google.com" id="internetLink"><div class="desc"><b>Visit the Website!</b></div><img src="./internet.png" class="internet"></a></div></div></div>');
 
   console.log(project.website);
 
@@ -23,14 +22,14 @@ function createProjectHtml(project) {
   $blankproject.find('a#githubLink').attr('href', project.github);
 
   if (project.website === null) {
-    $blankproject.find('div.galleryInternetGallery').remove();
+    $blankproject.find('div.internetGallery').remove();
   }
 
   return $blankproject;
 }
 
 function createTotalHtml(htmlArray){
-  var $totalHtml = $('<div>');
+  var $totalHtml = $('<div class="container">');
   var rowCounter = 0;
   var $newRow = $('<div class="row">');
 
@@ -54,7 +53,13 @@ function createTotalHtml(htmlArray){
   return $totalHtml;
 }
 
-rawProjectArray.push(createProject("My First Portfoilio", "My First Portfolio project utilized a simple layout to introduce myself as a programmer to the world. Highlights include the 'pen-and paper' style heading, as well as custom css on the introductory photo, as well as  a card layout with a hover mechanic in the sample future project section. The technologies employed during the develoment include HTML, CSS, and Bootstrap.", "https://github.com/MagicalRobotUnicorn/Basic-Portfolio", "https://magicalrobotunicorn.github.io/Basic-Portfolio/"));
+rawProjectArray.push(createProject("Lord of the Rings Quiz Game", "Test your Lord of Rings knowledge in a quiz game fit for the King of Gondor! The questions for this project were taken from the LOTR Wikipedia page and scraped from the website using the Node JS package Cheerio. After each question, a random positive or negative LOTR GIF is shown to the user delivered via the Giphy REST API. Notable features on this website include custom LOTR themed font effects intended to emulate the lettering present on the One Ring, as well as a custom countdown timer object CSS and jQuery. <br /><b>The technologies employed in this project include HTML, CSS, Javascript, jQuery, and REST APIs.</b>", "https://github.com/MagicalRobotUnicorn/quiz-game/", "https://magicalrobotunicorn.github.io/quiz-game/"));
+rawProjectArray.push(createProject("My First Portfoilio", "My first Portfolio utilized a simple layout to introduce myself as a programmer to the world. Highlights include the 'pen-and-paper' style attributed the heading through Google Web Font, the CSS applied to the cover photo as well as a drop shadow hover mechanic applied to the project headings. <br /><b>The technologies utilized include HTML, CSS, and Bootstrap.</b>", "https://github.com/MagicalRobotUnicorn/Basic-Portfolio", "https://magicalrobotunicorn.github.io/Basic-Portfolio/"));
+rawProjectArray.push(createProject("Star Wars jQuery Game", "The Star Wars jQuery game was a project that developed along a strict set of rules The player chooses a character, and after each successful round the player's attack is doubled. The site features a 'Star Wars' themed design, complete with custom logos, and an introductory video poking fun at out professor and our classes swift pace of development. Make sure to turn pop-ups on! <br /> <b>Technologies employed include HTML, CSS, Javascript, as well as jQuery.</b>", "https://github.com/MagicalRobotUnicorn/unit-4-game", "https://magicalrobotunicorn.github.io/unit-4-game/ "));
+rawProjectArray.push(createProject("Heavy Metal Word Guess Game", "Do you like heavy metal? Do you like quiz games? Well then, this website is for you! The website track keypress events and allows the user to play a hangman style game where the answers are all metal music artists. Upon completion, the user is shown a photo and an audio sample from the artist in question. This website features a 'metal' themed logo and asset either, all of which are either my own design or drawn from material in the public domain. The Google Font faces were chosen to match the aesthetic as well. <br /><b>The technologies employed include HTML, CSS, Javascript, and jQuery.</b>", "https://github.com/MagicalRobotUnicorn/Word-Guess-Game", "https://magicalrobotunicorn.github.io/Word-Guess-Game/"));
+rawProjectArray.push(createProject("Giftastic: Harry Potter", "There is a new course being offered at Hogwarts School of Witchcraft and Wizardry... Magical GIFs! This site utilizes REST APIs in order to provide the user a unique experience. After the user “prepares” a spell, they can then use it display an assortment of GIFs from Giphy's Rest API. The user  can then “sort” themselves using information drawn from both the Harry Potter API and Giphy. The site features  Harry Potter theme, with logos of my own design, as well as a series of font faces the fit the overall design goal. <br /><b>The technologies utilized include HTML, CSS, Javascript, jQuery, and REST APIs.</b>", "https://github.com/MagicalRobotUnicorn/giftastic", "https://magicalrobotunicorn.github.io/giftastic/"));
+rawProjectArray.push(createProject("Train Scheduler", "Hop aboard Magical railways to reach your ultimate destination! This assignment utilizes a cloud based database, Google Firestore, in order to provide the user a fictional schedule. The next train arrival time is calculated with MomentJS, a Javascript library that allows for the easy manipulation of datetime objects. Each train stop relates to the location in the children's game Candyland, but feel free to add your own! Once a train is added, the database will update in real time and the information will be accessible by every visitor to the site <br /> <b>The technologies employed include HTML, CSS, Javascript, jQuery, the MomentJS NPM Package, and Google Firebase.</b>", "https://github.com/MagicalRobotUnicorn/trainScheduler", "https://magicalrobotunicorn.github.io/trainScheduler/"));
+
 
 for (var i = 0; i < rawProjectArray.length; i++){
   completedProjects.push(createProjectHtml(rawProjectArray[i]));
@@ -62,4 +67,4 @@ for (var i = 0; i < rawProjectArray.length; i++){
 
 console.log(completedProjects.length);
 
-$('#putHere').append(createTotalHtml(completedProjects));
+$('#projectContents').append(createTotalHtml(completedProjects));
